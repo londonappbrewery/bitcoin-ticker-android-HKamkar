86d988590f1b94c872d3caadc868ab8e066f8165
Hossein Kamkar <soosha.com@gmail.com>
2017-07-26 10:43:47 +0430

    Finalizing App

--- app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
@@ -9,71 +9,90 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.Toast;
 
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.ResponseHandlerInterface;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 
-
 public class MainActivity extends AppCompatActivity {
 
-    // Constants:
-    // TODO: Create the base URL
-    private final String BASE_URL = "https://apiv2.bitcoin ...";
+  // Constants:
+  // TODO: Create the base URL
+  private final String BASE_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD";
 
-    // Member Variables:
-    TextView mPriceTextView;
+  // Member Variables:
+  TextView mPriceTextView;
+  private String symbol;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-        mPriceTextView = (TextView) findViewById(R.id.priceLabel);
-        Spinner spinner = (Spinner) findViewById(R.id.currency_spinner);
+    mPriceTextView = (TextView) findViewById(R.id.priceLabel);
+    Spinner spinner = (Spinner) findViewById(R.id.currency_spinner);
 
-        // Create an ArrayAdapter using the String array and a spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.currency_array, R.layout.spinner_item);
+    // Create an ArrayAdapter using the String array and a spinner layout
+    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+      R.array.currency_array, R.layout.spinner_item);
 
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
+    // Specify the layout to use when the list of choices appears
+    adapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
 
-        // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
+    // Apply the adapter to the spinner
+    spinner.setAdapter(adapter);
 
-        // TODO: Set an OnItemSelected listener on the spinner
+    // TODO: Set an OnItemSelected listener on the spinner
+    spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+      @Override
+      public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        symbol = "BTC" + parent.getItemAtPosition(position).toString();
+        RequestParams params = new RequestParams();
+        params.put("market", "global");
+        params.put("symbol", symbol);
+        Log.d("Bitcoin", symbol + " is selected");
+        letsDoSomeNetworking(params);
+      }
 
-    }
+      @Override
+      public void onNothingSelected(AdapterView<?> parent) {
+        Log.d("Bitcoin", "Nothing Selected");
+      }
+    });
+  }
 
-    // TODO: complete the letsDoSomeNetworking() method
-    private void letsDoSomeNetworking(String url) {
+  // TODO: complete the letsDoSomeNetworking() method
+  private void letsDoSomeNetworking(RequestParams params) {
+    AsyncHttpClient client = new AsyncHttpClient();
+    client.get(BASE_URL, params, new JsonHttpResponseHandler() {
 
-//        AsyncHttpClient client = new AsyncHttpClient();
-//        client.get(WEATHER_URL, params, new JsonHttpResponseHandler() {
-//
-//            @Override
-//            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-//                // called when response HTTP status is "200 OK"
-//                Log.d("Clima", "JSON: " + response.toString());
-//                WeatherDataModel weatherData = WeatherDataModel.fromJson(response);
-//                updateUI(weatherData);
-//            }
-//
-//            @Override
-//            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject response) {
-//                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-//                Log.d("Clima", "Request fail! Status code: " + statusCode);
-//                Log.d("Clima", "Fail response: " + response);
-//                Log.e("ERROR", e.toString());
-//                Toast.makeText(WeatherController.this, "Request Failed", Toast.LENGTH_SHORT).show();
-//            }
-//        });
+      @Override
+      public void onSuccess(int statusCode, cz.
+        msebera.android.httpclient.Header[] headers, JSONObject response) {
+        // called when response HTTP status is "200 OK"
+        Log.d("Bitcoin", "JSON: " + response.toString());
+        try {
+          mPriceTextView.setText(response.getString("last"));
+        } catch (JSONException e) {
+          e.printStackTrace();
+        }
+      }
+
+      @Override
+      public void onFailure(int statusCode, cz.
+        msebera.android.httpclient.Header[] headers, Throwable throwable, JSONObject errorResponse) {
+        super.onFailure(statusCode, headers, throwable, errorResponse);
+      }
+    });
 
 
-    }
+  }
 
 
 }
